82ad92bc074e47824197c171dd5f361bbc996453
choiyoung69 <choiyoung609@gmail.com>
2025-12-06 02:55:46 +0900

    [Feat] #10 - 상품 관련 repository 생성

--- baro-buyer/src/main/java/com/barofarm/buyer/product/domain/ProductRepository.java
+++ baro-buyer/src/main/java/com/barofarm/buyer/product/domain/ProductRepository.java
@@ -1,3 +1,16 @@
 package com.barofarm.buyer.product.domain;
 
-public class ProductRepository {}
+import java.util.Optional;
+import java.util.UUID;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface ProductRepository {
+  Page<Product> findAll(Pageable pageable);
+
+  Optional<Product> findById(UUID id);
+
+  Product save(Product product);
+
+  void deleteById(UUID id);
+}
