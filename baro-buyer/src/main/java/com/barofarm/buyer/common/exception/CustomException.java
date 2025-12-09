1d65d7ab68fdfd7dc36a2d15cf19126cffbdcec3
sesil <chhezue@gmail.com>
2025-12-09 16:47:18 +0900

    [Feat] #9 - buyer 모듈에 상훈님 common 패키지 추가

---
+++ baro-buyer/src/main/java/com/barofarm/buyer/common/exception/CustomException.java
@@ -0,0 +1,13 @@
+package com.barofarm.buyer.common.exception;
+
+import lombok.Getter;
+
+@Getter
+public class CustomException extends RuntimeException {
+    private final BaseErrorCode errorCode;
+
+    public CustomException(BaseErrorCode error) {
+        super(error.getMessage());
+        this.errorCode = error;
+    }
+}
---
+++ baro-buyer/src/main/java/com/barofarm/buyer/common/response/CustomPage.java
@@ -0,0 +1,39 @@
+package com.barofarm.buyer.common.response;
+
+import java.util.List;
+import org.springframework.data.domain.Page;
+
+public record CustomPage<T>(
+
+    List<T> content,
+
+    int page,
+
+    int size,
+
+    long totalElements,
+
+    int totalPages,
+
+    boolean first,
+
+    boolean last,
+
+    boolean hasNext,
+
+    boolean hasPrevious
+) {
+    public static <T> CustomPage<T> from(Page<T> page) {
+        return new CustomPage<>(
+            page.getContent(),
+            page.getNumber(),
+            page.getSize(),
+            page.getTotalElements(),
+            page.getTotalPages(),
+            page.isFirst(),
+            page.isLast(),
+            page.hasNext(),
+            page.hasPrevious()
+        );
+    }
+}
