c7dba38327dc3cdcb541be3f0560908e9e0d49d9
LAPTOP-K37MMFLI\stran <strangehoon2@gmail.com>
2025-12-11 09:14:36 +0900

    [Chore] #17 - 불필요한 주석 제거

--- baro-order/src/main/java/com/barofarm/order/payment/presentation/PaymentController.java
+++ baro-order/src/main/java/com/barofarm/order/payment/presentation/PaymentController.java
@@ -11,13 +11,12 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("${api.v1}/payments")
 @RequiredArgsConstructor
 // TODO: 나중에 인증/인가 붙이면 @RequestHeader("userId") UUID sellerId 사용
-public class PaymentController {
+public class PaymentController implements PaymentSwaggerApi{
 
     private final PaymentService paymentService;
 
     @PostMapping("/toss/confirm")
     public ResponseDto<TossPaymentConfirmInfo> confirmPayment(@RequestBody TossPaymentConfirmRequest confirmRequest){
-        System.out.println("de");
         return paymentService.confirmPayment(confirmRequest.toCommand());
     }
 }
