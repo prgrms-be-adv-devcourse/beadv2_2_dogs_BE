8eb5fd2c3c18452a39d91fd2f9a3261c8be38db1
LAPTOP-K37MMFLI\stran <strangehoon2@gmail.com>
2025-12-14 07:43:44 +0900

    [Chore] #17 - port 수정

--- baro-order/src/main/java/com/barofarm/order/order/client/InventoryClient.java
+++ baro-order/src/main/java/com/barofarm/order/order/client/InventoryClient.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient(
     name = "inventory-service",
-    url = "http://localhost:8080/api/v1/inventories",
+    url = "http://localhost:8082/api/v1/inventories",
     configuration = InventoryFeignConfig.class
 )
 public interface InventoryClient {
