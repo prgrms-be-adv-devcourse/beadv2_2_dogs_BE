95986df5aa60fc7ab6cda5aca5f62d305bfbdf2a
dev2_team02_dogs_danny <tjkueon0309@gmail.com>
2025-12-13 13:08:02 +0900

    [Fix] - JpaAuditingConfig 파일 충돌 해결

--- baro-buyer/src/main/java/com/barofarm/buyer/common/entity/JpaAuditingConfig.java
+++
@@ -1,8 +0,0 @@
-package com.barofarm.buyer.common.entity;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
-@Configuration
-@EnableJpaAuditing
-public class JpaAuditingConfig {}
