e66569a3c26efdfe3cda2620f72dc44c662aa89a
dev2_team02_dogs_danny <tjkueon0309@gmail.com>
2025-12-14 22:02:22 +0900

    [Fix] #22 - feign 클라이언트 수정

--- baro-support/src/main/java/com/barofarm/support/SupportApplication.java
+++ baro-support/src/main/java/com/barofarm/support/SupportApplication.java
@@ -7,7 +7,7 @@ import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
 @EnableKafka
-@EnableFeignClients(basePackages = "com.barofarm.support.common.client")
+@EnableFeignClients(basePackages = "com.barofarm.support")
 public class SupportApplication {
 
     public static void main(String[] args) {
