1e725221e0e7d6e6558c48d70603007674322d36
choiyoung69 <choiyoung609@gmail.com>
2025-12-16 18:04:18 +0900

    [Feat] #67 - Settlement Feign 통신 error decoder 생성

--- baro-support/src/main/java/com/barofarm/support/settlement/exception/SettlementErrorCode.java
+++ baro-support/src/main/java/com/barofarm/support/settlement/exception/SettlementErrorCode.java
@@ -9,7 +9,8 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum SettlementErrorCode implements BaseErrorCode {
 
-    SETTLEMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 월의 정산 내역이 존재하지 않습니다.");
+    SETTLEMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 월의 정산 내역이 존재하지 않습니다."),
+    ORDER_ITEM_NOT_FOUND(HttpStatus.NOT_FOUND, "주문 상품을 찾을 수 없습니다.");
 
     private final HttpStatus status;
     private final String message;
