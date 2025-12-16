9a5805fb7765dcca758c1594420900e1b278ca15
choiyoung69 <choiyoung609@gmail.com>
2025-12-16 11:37:03 +0900

    [Feat] #62 - commong config 생성

--- baro-support/src/main/java/com/barofarm/support/common/config/JacksonConfig.java
+++ baro-support/src/main/java/com/barofarm/support/common/config/JacksonConfig.java
@@ -6,16 +6,17 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
 @Configuration
 public class JacksonConfig {
 
     @Bean
     @Primary
-    public ObjectMapper objectMapper() {
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.registerModule(new JavaTimeModule());
-        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-        return mapper;
+    public ObjectMapper objectMapper(Jackson2ObjectMapperBuilder builder) {
+        return builder
+            .featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+            .modules(new JavaTimeModule())
+            .build();
     }
 }
